bbc498146416157e9885d513326ff248ea4f4e08
Ben Halverson <benhalverson33@gmail.com>
2024-12-04 03:36:01 -0800

    update product details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,31 +1,85 @@
 import { Component } from '@angular/core';
-import { Product, ProductResponse, ProductService } from '../product.service';
-import { ActivatedRoute } from '@angular/router';
+import { Product, ProductService } from '../product.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { catchError, of, Subject, switchMap, takeUntil, tap } from 'rxjs';
+import { FormBuilder, FormGroup, FormsModule, NgModel, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-    selector: 'app-product-details',
-    imports: [],
-    templateUrl: './product-details.component.html',
-    styleUrl: './product-details.component.css'
+  selector: 'app-product-details',
+  imports: [ReactiveFormsModule],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent {
 
-  constructor(private readonly productService: ProductService, private readonly route: ActivatedRoute) { }
-
+  productForm: FormGroup
   productDetails = {} as Product;
   editableDetails = {} as Product;
   isEditing = false;
+  private destroy$ = new Subject<void>();
 
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.productService.getProductById(params['id']).subscribe((product: ProductResponse) => {
-        this.productDetails = product;
-      });
+
+  constructor(
+    private readonly productService: ProductService,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly fb: FormBuilder,
+    private readonly toastr: ToastrService
+  ) {
+    this.productForm = this.fb.group({
+      name: [''],
+      description: ['', ],
+      filamentType: [''],
     });
   }
 
-  saveChanges() {
-    this.productService.updateProduct(this.productDetails).subscribe(() => {});
+  ngOnInit() {
+    this.route.params
+      .pipe(
+        switchMap((params: Params) =>
+          this.productService.getProductById(params['id'])
+        ),
+        tap((product) => {
+          this.productDetails = product;
+          this.productForm.patchValue(product);
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
   }
 
+  saveChanges() {
+    if(this.productForm.invalid) {
+      return;
+    }
+
+    const updatedProduct: Product = {
+      ...this.productDetails,
+      ...this.productForm.value,
+    }
+
+    this.productService
+      .updateProduct(updatedProduct)
+      .pipe(
+        tap(() => {
+          this.productDetails = updatedProduct;
+          this.isEditing = false;
+          this.toastr.success('Product updated successfully');
+          this.router.navigate(['/products']);
+        }),
+        catchError((error) => {
+          this.toastr.error('Error updating product');
+          // this.router.navigate(['/']);
+          return of(error);
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
